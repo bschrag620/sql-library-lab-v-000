cab8bc53d1cd4a5d5e824e3812b4fc4ebd395742
Brad Schrag <brad.schrag@gmail.com>
2019-01-04 13:49:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,26 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM Books
+   WHERE books.series_id = 1
+   ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM Characters
+   ORDER BY LENGTH(motto) DESC
+   LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM Characters 
+   GROUP BY species 
+   ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT Authors.name, Subgenres.name
+   FROM Authors JOIN (Series JOIN Subgenres ON Subgenres.id = Series.subgenre_id)
+   ON Authors.id = Series.author_id;"
 end
 
 def select_series_title_with_most_human_characters
